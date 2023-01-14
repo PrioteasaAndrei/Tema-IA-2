68339c3f14518e4f4e51a3ed3e540a724ea42186
Prio <cristi.prioteasa@stud.acs.upb.ro>
2023-01-14 14:49:57 +0200

    asa merge

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## Tema 2 IA
 
-1.3 1.4 nu par in regula
+https://towardsdatascience.com/regression-trees-from-scratch-in-30-lines-of-python-8196e0ab29b2
+https://towardsdatascience.com/decision-tree-algorithm-in-python-from-scratch-8c43f0e40173
+https://towardsdatascience.com/regression-tree-in-python-from-scratch-9b7b64c815e3
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 
+import pandas as pd
+
 a = {1:2,3:4}
 
 def func(dict):
@@ -7,3 +9,16 @@ def func(dict):
 print(a)
 func(a)
 print(a)
+
+
+df1 = pd.DataFrame({'key': ['foo', 'bar', 'baz', 'foo'],
+
+                    'value': [1, 2, 3, 5]})
+
+df2 = pd.DataFrame({'key': ['foo', 'bar', 'baz', 'foo'],
+
+                    'value': [5, 6, 7, 8]})
+
+
+df3 = pd.DataFrame()
+print(pd.concat([df1,df3]).reset_index(drop=True))
